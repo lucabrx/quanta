948ca607279e3bbf0b82e9dacbf437865058e08a
Luka Brkovic <coding@lbrkovic.com>
2023-06-11 18:46:11 +0200

    removed supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
 	import Footer from "$lib/components/Footer.svelte";
 	import Navbar from "$lib/components/Navbar.svelte";
-	import { onMount } from "svelte";
     import "../app.css"
-	import type { LayoutData } from "./$types";
-	import { invalidate } from "$app/navigation";
-
-    export let data: LayoutData;
-    $: ({session,supabase} = data) 
-
-    onMount(() => {
-        const {
-        data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, _session) => {
-        if (_session?.expires_at !== session?.expires_at) {
-            invalidate("supabase:auth")
-        }
-    })
-    return () => subscription.unsubscribe()
-    })
 </script>
 
 <svelte:head>
