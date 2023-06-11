f49248412ae48bd16f73ef3d8603c8d7bf0d8f48
Luka Brkovic <coding@lbrkovic.com>
2023-06-11 10:23:16 +0200

    initial styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Footer from "$lib/components/Footer.svelte";
+	import Navbar from "$lib/components/Navbar.svelte";
+import "../app.css"
+</script>
+
+<svelte:head>
+    <title>My App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    
+</svelte:head>
+
+<div  class="min-h-screen flex flex-col justify-between items-center bg-slate-50">
+<Navbar />
+<div class="f-full self-stretch flex-grow">
+<slot />
+</div>
+<Footer />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Button from "$lib/components/ui/Button.svelte";
+
+</script>
+
+<section class='relative isolate md:pt-14 pt-8'>
+	<div class='absolute inset-x-0 -top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+	<div
+	class='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] element'/>
+	</div>
+	<div class="md:py-20 md:mt-20 z-[100] px-4 lg:px-8">
+		<div class="mx-auto mb-10 max-w-2xl text-center">
+			<h1 class="mb-6 text-4xl sm:text-6xl">
+				<span class="text-transparent bg-clip-text bg-gradient-to-br from-[#e64c8c] to-[#645cd8] ">
+				The Next-Gen 
+				</span> 
+				 No idea for what <span class="text-transparent bg-clip-text bg-gradient-to-br from-[#e64c8c] to-[#645cd8] ">App</span> </h1>
+			<p class="text-lg">
+				Revolutionize your something game with noname - the ultimate solution for something
+				and nothing but yaaa.
+			</p>
+		</div>
+		<div class="flex items-center justify-center gap-3">
+			<Button size="lg" variant="primary">Get Started</Button>
+			<Button size="lg" variant="secondary">Learn More</Button>
+		</div>
+	</div>
+	</section>
+
+	
+
+
+
+<style>
+	.element {
+  clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);
+}
+
+</style>
